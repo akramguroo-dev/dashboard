d6bd5ddd10dcd1a0279306e7843a166533237e4c
akramguroo-dev <akramguroo.dev@gmail.com>
2026-07-14 16:27:07 +0530

    chore: configure Jest to handle TypeScript and JSX in test files

--- jest.config.js
+++ jest.config.js
@@ -6,4 +6,11 @@ export default {
   },
   setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
   testMatch: ['**/__tests__/**/*.ts?(x)', '**/?(*.)+(spec|test).ts?(x)'],
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', {
+      tsconfig: {
+        jsx: 'react-jsx',
+      },
+    }],
+  },
 };
